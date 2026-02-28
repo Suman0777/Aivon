eb1eac467937811b2dc380abc79e197b1b7512c0
suman basak <gorangobasakpkr@gmail.com>
2026-03-01 04:36:23 +0530

    Completed the sign and signin route

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,7 +1,10 @@
 import express from "express"
 const routes = express.Router();
-import Zod from "zod"
+import Zod, { email } from "zod"
 import User from "../db.js";
+import JWT from "jsonwebtoken";
+
+{/* This is for the Sign-UP */}
 
 const SignUPbody = Zod.object({
     name: Zod.string(),
@@ -9,7 +12,8 @@ const SignUPbody = Zod.object({
     password: Zod.string()
 })
 
-routes.post("/Signup", (req, res)=>{
+routes.post("/signup", async (req, res)=>{
+    const {name, email, password} = req.body
     const {success} = SignUPbody.safeParse(req.body)
     
     try {
@@ -17,8 +21,83 @@ routes.post("/Signup", (req, res)=>{
             return res.status(404).json({ msg: "Invalid Creadential" })
         }
         
+        const exsistingUser = await User.findOne({
+            email
+        })
+
+        if(exsistingUser) {
+            res.status(401).json({
+                msg: "User Already Exists!"
+            })
+        }
+
+        // bd user
+        const Users = await User.create({
+            name,
+            email,
+            password
+        })
+
+
+        const UserId = Users._id
+
+        const token = JWT.sign({
+            UserId
+        }, process.env.JWT_SECRET)
+
+        res.status(200).json({
+            msg: "USer Created! ",
+            token: token
+        })
+    } 
+    catch (error) {
+        console.error(error.message);
+    }
+})
+
+
+{/* This is for the Signin */}
+
+
+const SignInbody = Zod.object({
+    email: Zod.string().email(),
+    password: Zod.string()
+})
+
+routes.post('/signin', async(req, res)=>{
+    const {email, password} = req.body
+    const Checks = SignInbody.safeParse(req.body)
+    
+    try {
+        if(!Checks.success){
+            return res.status(404).json({
+                msg: "Input is Invalid!"
+            })
+        }
+
+        const users = await User.findOne({
+            email,
+            password
+        })   
+
+        if(!users){
+            return res.status(401).json({
+                msg: "Invalid email or password!"
+            })
+        }
+
+        const token = JWT.sign({
+            userId: users._id
+        },process.env.JWT_SECRET)
+        
+        res.status(200).json({
+            token: token
+        })
     } catch (error) {
         console.error(error.message);
+        res.status(500).json({
+            msg: "Server error"
+        })
     }
 })
 
